80aeb4610ed6ed94a3e8638b3ce5c27484ca39cc
Mohammad Karimi <karimi.mohammad.iran@gmail.com>
2025-06-20 18:52:50 +0330

    Feat: add some feat to pacman class

--- src/scripts/pacman.js
+++ src/scripts/pacman.js
@@ -7,6 +7,19 @@ class Pacman {
         this.movePX = movePX
     }
     draw() {
+        // TODO : animate gif in canvas and change size img in canvas
+        // gifler Lib
+        
+        let img = new Image();
+        img.onload = function () {
+            ctx.drawImage(img, this.x, this.y); // رسم تصویر از نقطه (0,0)
+        };
+        img.src = "./src/images/Pacman.gif";
+        // img.height = this.height + "px !important"
+        img.style.width = "10px"
+        img.height = "20"
+        console.log("ok");
+        console.log(img);
 
     }
     move() {
@@ -16,3 +29,7 @@ class Pacman {
 
     }
 }
+
+// create new
+const pacman = new Pacman(100, 100, blockSize, blockSize, blockSize / 2)
+pacman.draw()
