3e982fe726dff307adc9bad5fe978134d48716e5
Mohammad Karimi <karimi.mohammad.iran@gmail.com>
2025-06-20 15:29:38 +0330

    feat: create pacman class

---
+++ src/scripts/pacman.js
@@ -0,0 +1,18 @@
+class Pacman {
+    constructor(x, y, width, height, movePX) {
+        this.x = x
+        this.y = y
+        this.width = width
+        this.height = height
+        this.movePX = movePX
+    }
+    draw() {
+
+    }
+    move() {
+
+    }
+    changeDir() {
+
+    }
+}
